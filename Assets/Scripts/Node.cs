aa0bdb24d39a0beaa4584758f47e56fc1a23e242
Joshua Randall <josh.randall16@gmail.com>
2017-05-22 21:10:44 -0400

    Importing Project
    -Importing Starter Project
    -Importing Assets

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	//Boolean for what is walkable
+	public bool walkable;
+
+	//The world position
+	public Vector3 worldPosition;
+
+	public Node()
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
