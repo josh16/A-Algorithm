37c6880532ea399f9531f039e9010e5cd6b74073
josh16 <josh.randall16@gmail.com>
2017-05-25 03:26:57 -0400

    Minor Fixes
    -Minor Fixes to the code.
    -Repositioned the game Object for the grid at (0,0,0)

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -6,6 +6,7 @@ public class Grid : MonoBehaviour {
 
     //Type of mask
     public LayerMask unwalkableMask;
+    
 
     //The size of the Grid
     public Vector2 gridWorldSize;
@@ -28,8 +29,8 @@ public class Grid : MonoBehaviour {
     void Start()
     {
         nodeDiameter = nodeRadius * 2; // Node radius is being set to the diameter
-        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
-        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
 
         //Call the Create grid method
         CreateGrid();
@@ -42,24 +43,25 @@ public class Grid : MonoBehaviour {
     //Function to generate the Grid
     void CreateGrid()
     {
-       
-        grid = new Node[gridSizeX, gridSizeY];
+       grid = new Node[gridSizeX, gridSizeY];
+        
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
 
         //For loop to generate the grid
         //Generate the X rows
         for (int x = 0; x < gridSizeX; x++)
         {
-            // Bottom left corner
-            Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y/2;
-            
             //Generate the Y Cols
             for (int y = 0; y < gridSizeY; y++)
             {
                 //
-                Vector3 worldPoint = worldBottomLeft + Vector3.forward * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-                
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+
                 //Collision Check
-                bool walkable = !(Physics.CheckSphere)
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius,unwalkableMask));
+
+                //Create a new node(Populate the grid with nodes)
+                grid[x, y] = new Node(walkable, worldPoint);
             }
         }
           
@@ -70,9 +72,21 @@ public class Grid : MonoBehaviour {
     //Function for creating the Gizmo 
     void OnDrawGizmos()
     {
-        //Creating the wire cube which uses X and y
+        //Creating the wire cube which uses X and Y
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
+      
+
+        //
+        if(grid != null)
+        {
+            foreach(Node n in grid)
+            {
+                //Setting the Gizmos colour. White: main colour, collison: Red colour
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+              Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter -.1f));
+            }
+        }
     }
 
 
